61b031f568d30949f99c40c1b9bc74b0031664af
rodrigo claro <zembrzuski@gmail.com>
2019-07-06 16:54:58 -0300

    first commit

---
+++ src/main/kotlin/com/zembruzski/rollingsnow/datawarehouse/DatawareHouseApplication.kt
@@ -0,0 +1,11 @@
+package com.zembruzski.rollingsnow.datawarehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatawareHouseApplication
+
+fun main(args: Array<String>) {
+	runApplication<DatawareHouseApplication>(*args)
+}
